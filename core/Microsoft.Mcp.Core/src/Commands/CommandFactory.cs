1cfad712b331055ca5ddface6c12fb5bfb8cf396
Connie Yau <conniey@microsoft.com>
2025-08-27 00:15:55 -0700

    Add Commands/*

---
+++ core/Microsoft.Mcp.Core/src/Commands/CommandFactory.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Mcp.Core.Commands;
+
+public class CommandFactory
+{
+    public CommandGroup RootGroup { get; set; } = new CommandGroup("test", "description");
+}
