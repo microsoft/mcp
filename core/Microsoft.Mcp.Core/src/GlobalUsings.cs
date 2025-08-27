d1c2c90d9d7ffd467f557142819e2279b7c33454
Connie Yau <conniey@microsoft.com>
2025-08-26 23:57:02 -0700

    Add Globalusings

---
+++ core/Microsoft.Mcp.Core/src/GlobalUsings.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+global using System.CommandLine;
+global using System.CommandLine.Parsing;
+global using System.Text.Json;
+global using Microsoft.Mcp.Core.Extensions;
+global using Microsoft.Mcp.Core.Models;
+global using Microsoft.Mcp.Core.Models.Command;
+global using ModelContextProtocol.Server;
