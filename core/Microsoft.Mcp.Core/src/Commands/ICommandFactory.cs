4f2ecd536663cba12e20fae4f09b5e30b22bdee4
Connie Yau <conniey@microsoft.com>
2025-09-02 10:44:59 -0700

    Add ICommandFactory

---
+++ core/Microsoft.Mcp.Core/src/Commands/ICommandFactory.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Mcp.Core.Commands;
+
+public interface ICommandFactory
+{
+    public char Separator { get; }
+
+    public CommandGroup RootGroup { get; }
+
+    public IReadOnlyDictionary<string, IBaseCommand> AllCommands { get; }
+
+    public IReadOnlyDictionary<string, IBaseCommand> GroupCommands(string[] groupNames);
+
+}
