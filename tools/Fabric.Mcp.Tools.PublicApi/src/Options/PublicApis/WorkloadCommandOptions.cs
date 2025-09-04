efb60bf3208e15eeb6846e150544f0f97e3af2e3
Tomer Wintner <tomerwintner@microsoft.com>
2025-09-04 14:37:00 +0300

    Minor renamings.

--- tools/Fabric.Mcp.Tools.PublicApi/src/Options/PublicApis/WorkloadCommandOptions.cs
+++ tools/Fabric.Mcp.Tools.PublicApi/src/Options/PublicApis/WorkloadCommandOptions.cs
@@ -2,7 +2,7 @@
 
 namespace Fabric.Mcp.Tools.PublicApi.Options.PublicApis
 {
-    public class GetWorkloadApisOptions : BaseFabricOptions
+    public class WorkloadCommandOptions : BaseFabricOptions
     {
         [JsonPropertyName(FabricOptionDefinitions.WorkloadTypeName)]
         public string? WorkloadType { get; set; }
