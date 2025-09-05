77061f280b2bf6009c32858acd2692736d120d12
anannya03 <aaniee09@gmail.com>
2025-09-05 11:20:01 -0700

    Fixed serviceStartCommand

--- core/Azure.Mcp.Core/src/Areas/Server/Commands/ServiceStartCommand.cs
+++ core/Azure.Mcp.Core/src/Areas/Server/Commands/ServiceStartCommand.cs
@@ -29,6 +29,7 @@ public sealed class ServiceStartCommand : BaseCommand
     private readonly Option<string[]?> _namespaceOption = ServiceOptionDefinitions.Namespace;
     private readonly Option<string?> _modeOption = ServiceOptionDefinitions.Mode;
     private readonly Option<bool?> _readOnlyOption = ServiceOptionDefinitions.ReadOnly;
+    private readonly Option<bool> _debugOption = ServiceOptionDefinitions.Debug;
     private readonly Option<bool> _enableInsecureTransportsOption = ServiceOptionDefinitions.EnableInsecureTransports;
 
     /// <summary>
@@ -64,6 +65,7 @@ public sealed class ServiceStartCommand : BaseCommand
         command.Options.Add(_namespaceOption);
         command.Options.Add(_modeOption);
         command.Options.Add(_readOnlyOption);
+        command.Options.Add(_debugOption);
         command.Options.Add(_enableInsecureTransportsOption);
     }
 
@@ -78,6 +80,8 @@ public sealed class ServiceStartCommand : BaseCommand
         string[]? namespaces = parseResult.GetValue(_namespaceOption);
         string? mode = parseResult.GetValue(_modeOption);
         bool? readOnly = parseResult.GetValue(_readOnlyOption);
+
+        var debug = parseResult.GetValue(_debugOption);
 
         if (!IsValidMode(mode))
         {
@@ -101,6 +105,7 @@ public sealed class ServiceStartCommand : BaseCommand
             Namespace = namespaces,
             Mode = mode,
             ReadOnly = readOnly,
+            Debug = debug,
             EnableInsecureTransports = enableInsecureTransports,
         };
 
@@ -153,6 +158,25 @@ public sealed class ServiceStartCommand : BaseCommand
                 logging.ClearProviders();
                 logging.ConfigureOpenTelemetryLogger();
                 logging.AddEventSourceLogger();
+
+                if (serverOptions.Debug)
+                {
+                    // Configure console logger to emit Debug+ to stderr so tests can capture logs from StandardError
+                    logging.AddConsole(options =>
+                    {
+                        options.LogToStandardErrorThreshold = LogLevel.Debug;
+                        options.FormatterName = Microsoft.Extensions.Logging.Console.ConsoleFormatterNames.Simple;
+                    });
+                    logging.AddSimpleConsole(simple =>
+                    {
+                        simple.ColorBehavior = Microsoft.Extensions.Logging.Console.LoggerColorBehavior.Disabled;
+                        simple.IncludeScopes = false;
+                        simple.SingleLine = true;
+                        simple.TimestampFormat = "[HH:mm:ss] ";
+                    });
+                    logging.AddFilter("Microsoft.Extensions.Logging.Console.ConsoleLoggerProvider", LogLevel.Debug);
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                }
             })
             .ConfigureServices(services =>
             {
