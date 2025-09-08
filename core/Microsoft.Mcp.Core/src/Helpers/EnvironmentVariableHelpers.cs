af7465dc3ae7c6969c6fc57caff7c619b71aef83
Connie Yau <conniey@microsoft.com>
2025-09-08 04:41:44 -0700

    Move EnvironmentVariableHelpers

---
+++ core/Microsoft.Mcp.Core/src/Helpers/EnvironmentVariableHelpers.cs
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Mcp.Core.Helpers;
+
+public static class EnvironmentHelpers
+{
+    public static bool GetEnvironmentVariableAsBool(string envVarName)
+    {
+        return Environment.GetEnvironmentVariable(envVarName) switch
+        {
+            "true" => true,
+            "True" => true,
+            "T" => true,
+            "1" => true,
+            _ => false
+        };
+    }
+}
