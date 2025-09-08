62f41fb9911f8a3142956d4b437e9f587eb27b7f
Connie Yau <conniey@microsoft.com>
2025-09-08 05:31:07 -0700

    Fix using statements.

--- core/Azure.Mcp.Core/src/Areas/Subscription/SubscriptionSetup.cs
+++ core/Azure.Mcp.Core/src/Areas/Subscription/SubscriptionSetup.cs
@@ -2,9 +2,9 @@
 // Licensed under the MIT License.
 
 using Azure.Mcp.Core.Areas.Subscription.Commands;
-using Azure.Mcp.Core.Commands;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Mcp.Core.Areas;
 
 namespace Azure.Mcp.Core.Areas.Subscription;
 
