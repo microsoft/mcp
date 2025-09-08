a73ce0d5a167ee9171faab5b3640da2a3a26d48d
Connie Yau <conniey@microsoft.com>
2025-09-08 05:50:36 -0700

    Move ITelemetryService. Make a new TelemetryConstant

---
+++ core/Microsoft.Mcp.Core/src/Services/Telemetry/TelemetryConstants.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Mcp.Core.Services.Telemetry;
+
+internal static class TelemetryConstants
+{
+    /// <summary>
+    /// Name of tags published.
+    /// </summary>
+    internal class TagName
+    {
+        public const string AzureMcpVersion = "Version";
+        public const string ClientName = "ClientName";
+        public const string ClientVersion = "ClientVersion";
+        public const string ErrorDetails = "ErrorDetails";
+        public const string EventId = "EventId";
+        public const string ToolName = "ToolName";
+    }
+
+    internal class ActivityName
+    {
+        public const string ListToolsHandler = "ListToolsHandler";
+        public const string ToolExecuted = "ToolExecuted";
+    }
+}
