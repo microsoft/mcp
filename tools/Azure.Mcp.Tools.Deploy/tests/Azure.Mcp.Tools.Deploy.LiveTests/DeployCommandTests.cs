c7411a3f47ebd7cc0d0a40e200f8d267108b3b64
anannya03 <aaniee09@gmail.com>
2025-09-08 07:54:02 -0700

    Resolved merge conflicts - 11

--- tools/Azure.Mcp.Tools.Deploy/tests/Azure.Mcp.Tools.Deploy.LiveTests/DeployCommandTests.cs
+++ tools/Azure.Mcp.Tools.Deploy/tests/Azure.Mcp.Tools.Deploy.LiveTests/DeployCommandTests.cs
@@ -1,4 +1,3 @@
-/*
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
@@ -169,5 +168,3 @@ public class DeployCommandTests(ITestOutputHelper output) : CommandTestsBase(out
     }
 
 }
-
-*/
