1697116695d8857a75284e2a3f2b182b81feb836
Jair Myree <jairmyree@microsoft.com>
2025-09-17 13:41:39 -0400

    Updating test-resources-post and adding e2e prompt

--- tools/Azure.Mcp.Tools.EventHubs/tests/test-resources-post.ps1
+++ tools/Azure.Mcp.Tools.EventHubs/tests/test-resources-post.ps1
@@ -16,10 +16,4 @@ $testSettings = New-TestSettings @PSBoundParameters -OutputPath $PSScriptRoot
 # Create EventHub namespace and EventHub
 $eventHubNamespaceName = $BaseName
 $eventHubName = "test-hub"
-
-# Add EventHub details to test settings
-$testSettings["EVENTHUB_NAMESPACE_NAME"] = $eventHubNamespaceName
-$testSettings["EVENTHUB_NAME"] = $eventHubName
-$testSettings["EVENTHUB_CONNECTION_STRING"] = (Get-AzEventHubKey -ResourceGroupName $ResourceGroupName -NamespaceName $eventHubNamespaceName -AuthorizationRuleName "RootManageSharedAccessKey").PrimaryConnectionString
-
 Write-TestSettings -TestSettings $testSettings -OutputPath $PSScriptRoot
