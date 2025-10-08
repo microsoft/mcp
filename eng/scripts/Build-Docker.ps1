0b49c834e414f212091b379728ca77d260f8a8f8
Connie <conniey@microsoft.com>
2025-10-08 13:24:18 -0500

    Update build-docker file

--- eng/scripts/Build-Docker.ps1
+++ eng/scripts/Build-Docker.ps1
@@ -7,8 +7,32 @@ param(
     [Parameter(Mandatory=$true)]
     [string] $ServerName,
     [switch] $Trimmed,
-    [switch] $DebugBuild
+    [switch] $DebugBuild,
+    [Parameter(Mandatory=$true, ParameterSetName='SpecificPlatform')]
+    [ValidateSet('windows','linux','macOS')]
+    [string] $OperatingSystem,
+    [Parameter(Mandatory=$true, ParameterSetName='SpecificPlatform')]
+    [ValidateSet('x64','arm64')]
+    [string] $Architecture,
+    [Parameter(ParameterSetName='AllPlatforms')]
+    [switch] $AllPlatforms
 )
+
+function Get-DockerOS([string]$dotnetName) {
+    switch ($dotnetName) {
+        'linux' { return 'linux' }
+        'macOS' { return 'linux' }
+        'windows' { return 'win' }
+        default { return $dotnetName }
+    }
+}
+
+function Get-DockerArchitecture([string]$dotnetName) {
+    switch ($dotnetName) {
+        'x64' { return 'amd64' }
+        default { return $dotnetName }
+    }
+}
 
 . "$PSScriptRoot/../common/scripts/common.ps1"
 $root = $RepoRoot.Path.Replace('\', '/')
@@ -16,30 +40,72 @@ $distPath = "$root/.work"
 $dockerFile = "$root/Dockerfile"
 $properties = & "$PSScriptRoot/Get-ProjectProperties.ps1" -ProjectName "$ServerName.csproj"
 $dockerImageName = $properties.DockerImageName
+$buildDirectory = $([System.IO.Path]::Combine($distPath, "build", $ServerName))
 
 if(!$Version) {
     $Version = $properties.Version
 }
 
-# Will fix this when we update Dockerfile to multi-platform
-$os = "linux"
-$arch = "x64"
 $SingleFile = $Trimmed
 $tag = "$dockerImageName`:$Version$VersionSuffix";
 
-& "$root/eng/scripts/Build-Code.ps1" -ServerName $ServerName -VersionSuffix $VersionSuffix -SelfContained -Trimmed:$Trimmed -SingleFile:$SingleFile -DebugBuild:$DebugBuild -OperatingSystem $os -Architecture $arch
+if ($AllPlatforms) {
+    & "$root/eng/scripts/Build-Code.ps1" -ServerName $ServerName -VersionSuffix $VersionSuffix -SelfContained -Trimmed:$Trimmed -SingleFile:$SingleFile -DebugBuild:$DebugBuild -AllPlatforms
+
+    $operatingSystems = @('linux','windows')
+    $architectures = @('x64','arm64')
+
+} elseif ($OperatingSystem -and $Architecture) {
+    if ($OperatingSystem -eq 'macOS') {
+        $OperatingSystem = 'linux'
+    }
+
+    & "$root/eng/scripts/Build-Code.ps1" -ServerName $ServerName -VersionSuffix $VersionSuffix -SelfContained -Trimmed:$Trimmed -SingleFile:$SingleFile -DebugBuild:$DebugBuild -OperatingSystem $OperatingSystem -Architecture $Architecture
+
+    $operatingSystems = @($OperatingSystem)
+    $architectures = @($Architecture)
+} else {
+    Write-Error "Either specify both OperatingSystem and Architecture, or use the AllPlatforms switch."
+    exit 1
+}
+
 if ($LastExitCode -ne 0) {
     exit $LastExitCode
 }
 
-[string]$publishDirectory = $([System.IO.Path]::Combine($distPath, "build", $ServerName, "$os-$arch", "dist"))
-$relativeDirectory = $(Resolve-Path $publishDirectory -Relative).Replace('\', '/')
+# Move x64 folder to amd64 to match Docker naming conventions
+foreach ($os in $operatingSystems) {
+    
+}
 
-Write-Host "Building Docker image ($tag). PATH: [$relativeDirectory]. Absolute: [$publishDirectory]."
+[string[]]$identifiers = @()
+foreach($os in $operatingSystems) {
+    $dockerOS = Get-DockerOS $os
 
-if (!(Test-Path $publishDirectory)) {
-    Write-Error "Build output directory does not exist: $publishDirectory"
+    foreach($arch in $architectures) {
+        $dockerArchitecture = Get-DockerArchitecture $arch
+
+        if ($arch -eq 'x64') {
+            $x64Folder = "$buildDirectory/$dockerOS-x64"
+            $destination = "$buildDirectory/$dockerOS-amd64"
+            Write-Host "Moving $x64Folder to $destination" -ForegroundColor Yellow
+            Move-Item -Path $x64Folder -Destination $destination -Force
+        }
+
+        $identifiers += "$dockerOS/$dockerArchitecture"
+    }
+}
+
+# The COPY command needs to be within the scope of the "working directory". Usually where the ./Dockerfile is.  We need a relative path. 
+$relativeDirectory = $(Resolve-Path $buildDirectory -Relative).Replace('\', '/')
+
+if (!(Test-Path $buildDirectory)) {
+    Write-Error "Build output directory does not exist: $buildDirectory"
     return
 }
 
-& docker build --build-arg PUBLISH_DIR="$relativeDirectory" --file $dockerFile --tag $tag .
+[string]$platformString = [string]::Join(',', $identifiers)
+
+Write-Host "Building Docker image ($tag). PATH: [$relativeDirectory]. Absolute: [$buildDirectory].  Platforms: [$platformString]"
+
+& docker build --platform $platformString --build-arg PUBLISH_DIR="$relativeDirectory" --file $dockerFile --tag $tag .
