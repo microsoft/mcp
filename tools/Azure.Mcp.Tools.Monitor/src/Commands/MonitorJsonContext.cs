Azure/azure-mcp

Onboard Azure Monitor Web Tests tools

areas/monitor/src/AzureMcp.Monitor/Commands/MonitorJsonContext.cs
@@ -6,6 +6,7 @@ using System.Text.Json.Serialization;
 using AzureMcp.Monitor.Commands.Metrics;
 using AzureMcp.Monitor.Commands.Table;
 using AzureMcp.Monitor.Commands.TableType;
+using AzureMcp.Monitor.Commands.WebTests;
 using AzureMcp.Monitor.Commands.Workspace;
 
 namespace AzureMcp.Monitor.Commands;
@@ -16,6 +17,9 @@ namespace AzureMcp.Monitor.Commands;
 [JsonSerializable(typeof(TableTypeListCommand.TableTypeListCommandResult))]
 [JsonSerializable(typeof(MetricsQueryCommand.MetricsQueryCommandResult))]
 [JsonSerializable(typeof(MetricsDefinitionsCommand.MetricsDefinitionsCommandResult))]
+[JsonSerializable(typeof(WebTestsListCommand.WebTestsListCommandResult))]
+[JsonSerializable(typeof(WebTestsGetCommand.WebTestsGetCommandResult))]
+[JsonSerializable(typeof(WebTestsCreateOrUpdateCommand.WebTestsCreateCommandResult))]
 [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
 internal sealed partial class MonitorJsonContext : JsonSerializerContext
 {

microsoft/mcp

tools/Azure.Mcp.Tools.Monitor/src/Commands/MonitorJsonContext.cs
@@ -7,22 +7,29 @@ using Azure.Mcp.Tools.Monitor.Commands.ActivityLog;
 using Azure.Mcp.Tools.Monitor.Commands.Metrics;
 using Azure.Mcp.Tools.Monitor.Commands.Table;
 using Azure.Mcp.Tools.Monitor.Commands.TableType;
+using Azure.Mcp.Tools.Monitor.Commands.WebTests;
 using Azure.Mcp.Tools.Monitor.Commands.Workspace;
 using Azure.Mcp.Tools.Monitor.Models.ActivityLog;
 
 namespace Azure.Mcp.Tools.Monitor.Commands;
 
-[JsonSerializable(typeof(List<JsonNode>))]
-[JsonSerializable(typeof(WorkspaceListCommand.WorkspaceListCommandResult))]
-[JsonSerializable(typeof(TableListCommand.TableListCommandResult))]
-[JsonSerializable(typeof(TableTypeListCommand.TableTypeListCommandResult))]
-[JsonSerializable(typeof(MetricsQueryCommand.MetricsQueryCommandResult))]
-[JsonSerializable(typeof(MetricsDefinitionsCommand.MetricsDefinitionsCommandResult))]
+[JsonSerializable(typeof(ActivityLogEventData))]
+[JsonSerializable(typeof(ActivityLogEventLevel))]
 [JsonSerializable(typeof(ActivityLogListCommand.ActivityLogListCommandResult))]
 [JsonSerializable(typeof(ActivityLogListResponse))]
-[JsonSerializable(typeof(ActivityLogEventData))]
 [JsonSerializable(typeof(ActivityLogLocalizableString))]
-[JsonSerializable(typeof(ActivityLogEventLevel))]
+[JsonSerializable(typeof(List<JsonNode>))]
+[JsonSerializable(typeof(MetricsDefinitionsCommand.MetricsDefinitionsCommandResult))]
+[JsonSerializable(typeof(MetricsDefinitionsCommand.MetricsDefinitionsCommandResult))]
+[JsonSerializable(typeof(MetricsQueryCommand.MetricsQueryCommandResult))]
+[JsonSerializable(typeof(MetricsQueryCommand.MetricsQueryCommandResult))]
+[JsonSerializable(typeof(TableListCommand.TableListCommandResult))]
+[JsonSerializable(typeof(TableTypeListCommand.TableTypeListCommandResult))]
+[JsonSerializable(typeof(WebTestsCreateCommand.WebTestsCreateCommandResult))]
+[JsonSerializable(typeof(WebTestsGetCommand.WebTestsGetCommandResult))]
+[JsonSerializable(typeof(WebTestsListCommand.WebTestsListCommandResult))]
+[JsonSerializable(typeof(WebTestsUpdateCommand.WebTestsUpdateCommandResult))]
+[JsonSerializable(typeof(WorkspaceListCommand.WorkspaceListCommandResult))]
 [JsonSerializable(typeof(Dictionary<string, object>))]
 [JsonSerializable(typeof(Dictionary<string, object?>))]
 [JsonSerializable(typeof(object))]