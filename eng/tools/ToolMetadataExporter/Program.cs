762f874d1016b99e5b27b9ad41f208245464343b
Connie Yau <conniey@microsoft.com>
2025-10-23 15:02:33 -0700

    Add initial mock up

--- eng/tools/ToolMetadataExporter/Program.cs
+++ eng/tools/ToolMetadataExporter/Program.cs
@@ -1,48 +1,50 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Azure.Core;
+using Azure.Identity;
+using Kusto.Data;
+using Kusto.Data.Common;
+using Kusto.Data.Net.Client;
+using Kusto.Ingest;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using ToolMetadataExporter.Models.Kusto;
+using Microsoft.Extensions.Options;
 using ToolMetadataExporter.Services;
 
 namespace ToolMetadataExporter;
 
 public class Program
 {
-    private readonly AzmcpProgram _azmcpExe;
-    private readonly IAzureMcpDatastore _azureMcpDatastore;
-    private readonly ILogger<Program> _logger;
-
-    public Program(AzmcpProgram program, IAzureMcpDatastore azureMcpDatastore, ILogger<Program> logger)
-    {
-        _azmcpExe = program;
-        _azureMcpDatastore = azureMcpDatastore;
-        _logger = logger;
-    }
-
     public static async Task Main(string[] args)
     {
         var builder = Host.CreateApplicationBuilder(args);
-        ConfigureServices(builder.Services);
+        ConfigureServices(builder.Services, builder.Configuration);
+        ConfigureAzureServices(builder.Services);
 
         var host = builder.Build();
 
-        var program = host.Services.GetRequiredService<Program>();
+        var analyzer = host.Services.GetRequiredService<ToolAnalyzer>();
 
-        await program.RunAsync(DateTimeOffset.UtcNow, isDryRun: true);
+        await analyzer.RunAsync(DateTimeOffset.UtcNow, isDryRun: true);
         await host.RunAsync();
     }
 
-    private static void ConfigureServices(IServiceCollection services)
+    private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
+        services.AddLogging(builder =>
+        {
+            builder.AddConsole();
+        });
 
         services.AddSingleton<IAzureMcpDatastore, AzureMcpKustoDatastore>()
-            .AddSingleton<AzmcpProgram>();
+            .AddSingleton<AzmcpProgram>()
+            .AddSingleton<ToolAnalyzer>();
 
-        services.AddOptions<AppConfiguration>()
-            .PostConfigure(existing =>
+        services.Configure<AppConfiguration>(configuration.GetSection("AppConfig"))
+            .PostConfigure<AppConfiguration>(existing =>
             {
                 if (existing.WorkDirectory == null)
                 {
@@ -51,61 +53,33 @@ public class Program
                     existing.WorkDirectory = Path.Combine(repoRoot, ".work");
                 }
             });
-
-        services.AddLogging(builder =>
-        {
-            builder.AddConsole();
-        });
     }
 
-    private async Task RunAsync(DateTimeOffset analysisTime, bool isDryRun, CancellationToken cancellationToken = default)
+    private static void ConfigureAzureServices(IServiceCollection services)
     {
-        _logger.LogInformation("Starting analysis.");
+        services.AddScoped<TokenCredential>(sp => {
+            var credential = new ChainedTokenCredential(
+                new ManagedIdentityCredential(),
+                new DefaultAzureCredential()
+            );
 
-        var currentTools = await _azmcpExe.LoadToolsDynamicallyAsync();
-
-        if (currentTools == null)
+            return credential;
+        });
+        services.AddSingleton<ICslQueryProvider>(sp =>
         {
-            _logger.LogError("LoadToolsDynamicallyAsync did not return a result.");
-            return;
-        }
-        else if (currentTools.Tools == null || currentTools.Tools.Count == 0)
-        {
-            _logger.LogWarning("azmcp program did not return any tools.");
-            return;
-        }
+            var config = sp.GetRequiredService<IOptions<AppConfiguration>>();
+            var credential = sp.GetRequiredService<TokenCredential>();
+            var connectionStringBuilder = new KustoConnectionStringBuilder(config.Value.ClusterEndpoint)
+                .WithAadAzureTokenCredentialsAuthentication(credential);
 
-        var existingTools = (await _azureMcpDatastore.GetAvailableToolsAsync(cancellationToken)).ToDictionary(x => x.ToolId);
+            return KustoClientFactory.CreateCslQueryProvider(connectionStringBuilder);
+        });
+        services.AddSingleton<IKustoIngestClient>(sp => {
+            var config = sp.GetRequiredService<IOptions<AppConfiguration>>();
+            var credential = sp.GetRequiredService<TokenCredential>();
+            var connectionStringBuilder = new KustoConnectionStringBuilder(config.Value.ClusterEndpoint);
 
-        if (cancellationToken.IsCancellationRequested)
-        {
-            _logger.LogInformation("Analysis was cancelled.");
-            return;
-        }
-
-        var changes = new List<McpToolEvent>();
-
-        foreach (var tool in currentTools.Tools)
-        {
-            if (string.IsNullOrEmpty(tool.Id))
-            {
-                throw new InvalidOperationException($"Tool without an id. Name: {tool.Name}. Command: {tool.Command}");
-            }
-
-            if (!existingTools.TryGetValue(tool.Id, out var knownValue))
-            {
-               
-            }
-            else
-            {
-                changes.Add(new McpToolEvent
-                {
-                    EventTime = analysisTime,
-                    EventType = McpToolEventType.Created,
-                });
-            }
-        }
-
-        await _azureMcpDatastore.AddToolEventsAsync(changes, cancellationToken);
+            return KustoIngestFactory.CreateQueuedIngestClient(connectionStringBuilder);
+        });
     }
 }
