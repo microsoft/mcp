8aa39c36da2b9d4b9b7f34fdf8c81b2d1d1d30c9
Connie Yau <conniey@microsoft.com>
2025-11-07 10:41:51 -0800

    Add Options Validation.

---
+++ core/Azure.Mcp.Core/src/Configuration/AzureMcpServerConfigurationValidator.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Options;
+
+namespace Azure.Mcp.Core.Configuration;
+
+[OptionsValidator]
+public partial class AzureMcpServerConfigurationValidator : IValidateOptions<AzureMcpServerConfiguration>
+{
+}
