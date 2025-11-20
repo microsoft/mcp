693322aeab3bf609bb9e0e519be244ddbd0e3f4b
Xiang Yan <xiangsjtu@gmail.com>
2025-11-20 10:07:07 -0800

    remove unused method

--- core/Azure.Mcp.Core/src/Models/Elicitation/ElicitationSchema.cs
+++ core/Azure.Mcp.Core/src/Models/Elicitation/ElicitationSchema.cs
@@ -62,17 +62,4 @@ public static class ElicitationSchema
 
         return JsonSerializer.SerializeToNode(schema, ModelsJsonContext.Default.ElicitationSchemaRoot)!.AsObject();
     }
-
-    /// <summary>
-    /// Creates a secret value input schema for elicitation.
-    /// </summary>
-    /// <param name="propertyName">The name of the property to request.</param>
-    /// <param name="title">The display title for the property.</param>
-    /// <param name="description">The description of what the property represents.</param>
-    /// <param name="isRequired">Whether the property is required.</param>
-    /// <returns>A JSON schema object for the elicitation request.</returns>
-    public static JsonObject CreateSecretSchema(string propertyName, string title, string description, bool isRequired = true)
-    {
-        return CreatePasswordSchema(propertyName, title, description, isRequired);
-    }
 }
