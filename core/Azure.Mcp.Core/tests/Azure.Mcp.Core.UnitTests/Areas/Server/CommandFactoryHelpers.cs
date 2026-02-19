585ffe493deddb4c6907ae4809264eac8956444f
Alan Zimmer <48699787+alzimmermsft@users.noreply.github.com>
2026-02-19 09:21:13 -0500

    Add McpServerName to telemetry (#1755)
    * Add McpServerName to telemetry
    
    * Check for new telemetry tags in tests
    
    * Add CHANGELOG entry

--- core/Azure.Mcp.Core/tests/Azure.Mcp.Core.UnitTests/Areas/Server/CommandFactoryHelpers.cs
+++ core/Azure.Mcp.Core/tests/Azure.Mcp.Core.UnitTests/Areas/Server/CommandFactoryHelpers.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using Azure.Mcp.Core.Areas.Group;
 using Azure.Mcp.Core.Areas.Subscription;
 using Azure.Mcp.Core.Commands;
-using Azure.Mcp.Core.Configuration;
 using Azure.Mcp.Core.Services.Telemetry;
 using Azure.Mcp.Tools.Acr;
 using Azure.Mcp.Tools.Aks;
@@ -44,6 +43,7 @@ using Azure.Mcp.Tools.Workbooks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Mcp.Core.Areas;
+using Microsoft.Mcp.Core.Configuration;
 using ModelContextProtocol.Protocol;
 
 namespace Azure.Mcp.Core.UnitTests.Areas.Server;
@@ -96,7 +96,7 @@ internal class CommandFactoryHelpers
 
         var services = serviceProvider ?? CreateDefaultServiceProvider();
         var logger = services.GetRequiredService<ILogger<CommandFactory>>();
-        var configurationOptions = Microsoft.Extensions.Options.Options.Create(new AzureMcpServerConfiguration
+        var configurationOptions = Microsoft.Extensions.Options.Options.Create(new McpServerConfiguration
         {
             Name = "Test Server",
             Version = "Test Version",
