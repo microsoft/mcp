c2ff784adc5e24a02814771e1154c974acabea05
Chidozie Ononiwu <31145988+chidozieononiwu@users.noreply.github.com>
2026-02-23 13:45:05 -0800

    Run ServerCommandTests in http transport mode (#1783)
    * Switch ServerCommand To LiveTests
    
    * Fix ServerCommandTests.cs formating
    
    * Rename ServerCommandTests to ServerStartCommandTests

--- core/Azure.Mcp.Core/tests/Azure.Mcp.Tests/Client/CommandTestsBase.cs
+++ core/Azure.Mcp.Core/tests/Azure.Mcp.Tests/Client/CommandTestsBase.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
