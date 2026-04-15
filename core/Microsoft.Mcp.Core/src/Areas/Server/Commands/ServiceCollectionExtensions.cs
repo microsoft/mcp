95df70cb8bc5812d7905b9cacbaf18bf711900b5
alzimmermsft <48699787+alzimmermsft@users.noreply.github.com>
2026-04-15 13:32:32 -0400

    Fix linting

--- core/Microsoft.Mcp.Core/src/Areas/Server/Commands/ServiceCollectionExtensions.cs
+++ core/Microsoft.Mcp.Core/src/Areas/Server/Commands/ServiceCollectionExtensions.cs
@@ -275,7 +275,7 @@ public static class ServiceCollectionExtensions
 
                 // Assembly.GetEntryAssembly is used to retrieve the version of the server application as that is
                 // the assembly that will run the tool calls.
-                var entryAssembly = Assembly.GetEntryAssembly() 
+                var entryAssembly = Assembly.GetEntryAssembly()
                     ?? throw new InvalidOperationException("Entry assembly must be a managed assembly.");
 
                 options.Version = AssemblyHelper.GetAssemblyVersion(entryAssembly);
