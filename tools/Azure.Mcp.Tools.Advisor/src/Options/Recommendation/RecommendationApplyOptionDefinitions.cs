70bf428feff404cf94d9db951e8ad1bcc0737bf1
ankiga-MSFT <ankiga@microsoft.com>
2026-05-20 15:10:36 +0530

    Cleanup for unsupported scenario (Arch Diagrams)

--- tools/Azure.Mcp.Tools.Advisor/src/Options/Recommendation/RecommendationApplyOptionDefinitions.cs
+++ tools/Azure.Mcp.Tools.Advisor/src/Options/Recommendation/RecommendationApplyOptionDefinitions.cs
@@ -8,7 +8,7 @@ public static class RecommendationApplyOptionDefinitions
         $"--{ResourceType}"
     )
     {
-        Description = "The Azure resource type for which to get rules to apply to IaaC file or fetch recommendations for an architecture diagram. Available options: 'aad_domainservices', 'apimanagement_service', 'cognitiveservices_accounts', 'compute_virtualmachines', 'compute_virtualmachinescalesets', 'containerregistry_registries', 'containerservice_managedclusters', 'dbforpostgresql_flexibleservers', 'documentdb_databaseaccounts', 'keyvault_vaults', 'kubernetes_connectedclusters', 'kubernetesconfiguration_extensions', 'netapp_volumes', 'network_applicationgatewaywebapplicationfirewallpolicies', 'network_expressrouteports', 'network_frontdoorwebapplicationfirewallpolicies', 'sql_managedinstances', 'storage_storageaccounts', 'web_serverfarms', 'web_staticsites'",
+        Description = "The Azure resource type for which to get rules to apply to IaaC file. Available options: 'aad_domainservices', 'apimanagement_service', 'cognitiveservices_accounts', 'compute_virtualmachines', 'compute_virtualmachinescalesets', 'containerregistry_registries', 'containerservice_managedclusters', 'dbforpostgresql_flexibleservers', 'documentdb_databaseaccounts', 'keyvault_vaults', 'kubernetes_connectedclusters', 'kubernetesconfiguration_extensions', 'netapp_volumes', 'network_applicationgatewaywebapplicationfirewallpolicies', 'network_expressrouteports', 'network_frontdoorwebapplicationfirewallpolicies', 'sql_managedinstances', 'storage_storageaccounts', 'web_serverfarms', 'web_staticsites'",
         Required = true
     };
 }
