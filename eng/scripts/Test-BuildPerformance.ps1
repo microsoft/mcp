55cc3341ba58c128da2b915b03c4f5afa2e21fa2
Patrick Hallisey <pahallis@microsoft.com>
2026-05-22 12:12:24 -0700

    Improve Test-BuildPerformance script

--- eng/scripts/Test-BuildPerformance.ps1
+++ eng/scripts/Test-BuildPerformance.ps1
@@ -4,8 +4,7 @@
 param(
     [parameter(Mandatory)]
     [string] $ServerName,
-    [int] $RunCount = 3,
-    [int] $InvocationsPerRun = 10
+    [int] $RunCount = 10
 )
 
 . "$PSScriptRoot/../common/scripts/common.ps1"
@@ -17,12 +16,12 @@ $combinations += @{
 }
 
 $combinations += @{
-    Name = "trimmed/no-self"
+    Name = "single/no-self"
+    SingleFile = $true
 }
 
 $combinations += @{
-    Name = "trimmed r2r/no-self"
-    Trimmed = $true
+    Name = "r2r/no-self"
     ReadyToRun = $true
 }
 
@@ -55,19 +54,27 @@ $combinations += @{
 }
 
 function SaveResults() {
-    $csv = @("Name,Compilation,First Run,Average Run,Package Size")
+    $csv = @("Name,Self Contained,Trimmed,Single File,Ready To Run,Native,Compilation,First Run,Average Run,Package Size")
 
     foreach($combination in $combinations) {
-        $result = $results[$combination.Name]
+        $name = $combination.Name
+        $singleFile = !!$combination.SingleFile
+        $trimmed = !!$combination.Trimmed
+        $selfContained = !!$combination.SelfContained
+        $readyToRun = !!$combination.ReadyToRun
+        $native = !!$combination.Native
 
-        $compilation = ($result | Measure-Object -Property Compilation -Average).Average
-        $firstRun = ($result | Measure-Object -Property FirstRun -Average).Average
-        $averageRun = ($result | Measure-Object -Property AverageRun -Average).Average
-        $packageSize = ($result | Measure-Object -Property PackageSize -Average).Average
+        $result = $results[$name]
 
-        $csv += "$($combination.Name),$compilation,$firstRun,$averageRun,$packageSize"
+        $compilation = $result.Compilation
+        $firstRun = $result.FirstRun
+        $averageRun = $result.AverageRun
+        $packageSize = $result.PackageSize
+
+        $csv += "$name,$selfContained,$trimmed,$singleFile,$readyToRun,$native,$compilation,$firstRun,$averageRun,$packageSize"
     }
 
+    New-Item -ItemType Directory -Path .work -Force
     $csv | Out-File -FilePath .work/results.csv -Encoding utf8 -Force
 }
 
@@ -84,12 +91,24 @@ try {
         Write-Host "Building '$($combination.Name)' to '$outputPath' ..."
         Write-Host "-----------------------------------------------------------------------------------"
 
-        Remove-Item -Path .work -Recurse -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
-        Remove-Item -Path .dist -Recurse -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
-        Remove-Item -Path ./src/bin -Recurse -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
-        Remove-Item -Path ./src/obj -Recurse -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
+        Remove-Item -Path .work/build -Recurse -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
+
+        # Remove bin and obj directories anywhere in the repo
+        Get-ChildItem . -Directory -Recurse
+        | Where-Object Name -Match '^(bin|obj)$'
+        | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
 
         $start = Get-Date
+
+        Write-Host @"
+./eng/scripts/Build-Code.ps1 ``
+    -ServerName '$ServerName' ``
+    -SelfContained:`$$(!!$combination.SelfContained) ``
+    -Trimmed:`$$(!!$combination.Trimmed) ``
+    -ReadyToRun:`$$(!!$combination.ReadyToRun) ``
+    -SingleFile:`$$(!!$combination.SingleFile) ``
+    -Native:`$$(!!$combination.Native)
+"@
 
         ./eng/scripts/Build-Code.ps1 `
             -ServerName $ServerName `
@@ -101,6 +120,10 @@ try {
 
         $compilation = ((Get-Date) - $start).TotalMilliseconds
 
+        # Remove pdb files
+        Get-ChildItem -Path $outputPath -Filter "*.pdb"
+        | Remove-Item -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
+
         $executable = Get-ChildItem -Path $outputPath -Filter "*.exe" | Select-Object -First 1
 
         if(-not $executable) {
@@ -110,31 +133,29 @@ try {
 
         $command = "$($executable.FullName) tools list"
 
-        for($i = 1; $i -le $RunCount; $i++) {
-            Write-Host "Running '$($combination.Name)' - Round $i"
-            Write-Host "----------------------------------------"
+        Write-Host "Running '$($combination.Name)' - Round $i"
+        Write-Host "----------------------------------------"
 
+        $start = Get-Date
+
+        $runs = @()
+        Write-Host "Running $command"
+        foreach($x in 1..$RunCount) {
             $start = Get-Date
-
-            $runs = @()
-            Write-Host "Running $command"
-            foreach($x in 1..$InvocationsPerRun) {
-                $start = Get-Date
-                Invoke-Expression $command | Out-Null
-                if ($LASTEXITCODE -ne 0) {
-                    Write-Warning "$($combination.Name) failed with exit code $LASTEXITCODE"
-                }
-                $runs += ((Get-Date) - $start).TotalMilliseconds
+            Invoke-Expression $command | Out-Null
+            if ($LASTEXITCODE -ne 0) {
+                Write-Warning "$($combination.Name) failed with exit code $LASTEXITCODE"
             }
+            $runs += ((Get-Date) - $start).TotalMilliseconds
+        }
 
-            $packageSize = (Get-ChildItem -Path $executable.DirectoryName -File -Recurse | Measure-Object -Property Length -Sum).Sum / 1MB
+        $packageSize = (Get-ChildItem -Path $executable.DirectoryName -File -Recurse | Measure-Object -Property Length -Sum).Sum / 1MB
 
-            $results[$combination.Name] += @{
-                Compilation = $compilation
-                FirstRun = $runs[0]
-                AverageRun = ($runs | Select-Object -Skip 2 | Measure-Object -Average).Average
-                PackageSize = $packageSize
-            }
+        $results[$combination.Name] = @{
+            Compilation = $compilation
+            FirstRun = $runs[0]
+            AverageRun = ($runs | Select-Object -Skip 2 | Measure-Object -Average).Average
+            PackageSize = $packageSize
         }
 
         SaveResults
