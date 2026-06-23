be514ec461fc8089461bca767e09bc87bfe053ca
Connie Yau <conniey@microsoft.com>
2026-06-23 11:25:21 -0700

    Add support to parse prompts and output evals

---
+++ eng/tools/VallyEvaluator/src/Models/Stimulus.cs
@@ -0,0 +1,14 @@
+﻿namespace VallyEvaluator.Models;
+
+public class Stimulus
+{
+    public required string Name { get; set; }
+
+    public required string Prompt { get; set; }
+
+    public Dictionary<string, string>? Tags { get; set; }
+
+    public string? Environment { get; set; }
+
+    public List<StimulusGraderConfig>? Graders { get; set; }
+}
---
+++ eng/tools/VallyEvaluator/src/RunConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace VallyEvaluator;
+
+public class RunConfiguration
+{
+    [ConfigurationKeyName("output")]
+    public string OutputDirectory { get; set; } = string.Empty;
+
+    [ConfigurationKeyName("namespaces")]
+    public string NamespacesValue { get; set; } = string.Empty;
+
+    public List<string> Namespaces { get; set; } = new List<string>();
+
+    [ConfigurationKeyName("promptFile")]
+    public string PromptFilePath { get; set; } = string.Empty;
+
+    [ConfigurationKeyName("workingDirectory")]
+    public string WorkingDirectory { get; set; } = string.Empty;
+}
