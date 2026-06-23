1b9400c577c0df7a160bf842f30a727175148f8f
Connie Yau <conniey@microsoft.com>
2026-06-23 11:25:21 -0700

    Add support to parse prompts and output evals

---
+++ eng/tools/VallyEvaluator/src/Program.cs
@@ -0,0 +1,99 @@
+﻿using McpToolEvaluator.Core;
+using McpToolEvaluator.Core.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace VallyEvaluator;
+
+internal class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+        var runConfig = new RunConfiguration();
+        configuration.Bind(runConfig);
+
+        if (!string.IsNullOrEmpty(runConfig.NamespacesValue))
+        {
+            runConfig.Namespaces = [.. runConfig.NamespacesValue.Split(',')];
+        }
+
+        var outputDirectory = runConfig.OutputDirectory;
+        if (string.IsNullOrEmpty(outputDirectory))
+        {
+            Console.WriteLine("Output directory is required.");
+            return -1;
+        }
+
+        var repoRoot = Utilities.FindRepoRoot(AppContext.BaseDirectory);
+        if (string.IsNullOrEmpty(runConfig.WorkingDirectory))
+        {
+            runConfig.WorkingDirectory = Path.Join(repoRoot, ".work");
+        }
+
+        if (!Directory.Exists(runConfig.WorkingDirectory))
+        {
+            Directory.CreateDirectory(runConfig.WorkingDirectory);
+        }
+
+        try
+        {
+            await RunEvaluationAsync(repoRoot, runConfig);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error during evaluation: {ex.Message}");
+            Console.WriteLine(ex);
+            return -1;
+        }
+
+        return 0;
+    }
+
+    private static async Task RunEvaluationAsync(string repoRoot, RunConfiguration configuration)
+    {
+        string promptsPath = string.Empty;
+        if (string.IsNullOrEmpty(configuration.PromptFilePath))
+        {
+            promptsPath = Path.Combine(repoRoot, "servers", "Azure.Mcp.Server", "docs", "e2eTestPrompts.md");
+        }
+        else
+        {
+            promptsPath = Path.GetFullPath(configuration.PromptFilePath);
+        }
+
+        var promptDatastore = new PromptDatastore(promptsPath);
+        var vallyEvalDirectory = Path.Combine(configuration.WorkingDirectory, "vallyEvals");
+        Directory.CreateDirectory(vallyEvalDirectory);
+
+        foreach (var ns in configuration.Namespaces)
+        {
+            var prompts = promptDatastore.GetPromptsByNamespace(ns)
+                .OrderBy(p => p.Prompt)
+                .ToList();
+
+            var outputDirectory = Path.Combine(vallyEvalDirectory, ns);
+            Directory.CreateDirectory(outputDirectory);
+            var outputFile = Path.Combine(outputDirectory, "eval.yaml");
+
+            await VallyUtilities.WritePromptsAsync(prompts, outputFile, force: true);
+        }
+
+        //var evaluator = new VallyEvaluator();
+        //await evaluator.RunEvaluationAsync(outputDirectory);
+    }
+
+    private static async Task<TestPrompt[]> GetTestPromptAsync(PromptDatastore datastore)
+    {
+        var namespaces = datastore.GetPromptsByNamespace("");
+        Console.WriteLine($"Available namespaces ({namespaces.Count}):");
+        foreach (var ns in namespaces)
+        {
+            Console.WriteLine($"  - {ns}");
+        }
+
+        return [];
+    }
+}
---
+++ eng/tools/VallyEvaluator/src/VallyUtilities.cs
@@ -0,0 +1,81 @@
+﻿using McpToolEvaluator.Core.Models;
+using VallyEvaluator.Models;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace VallyEvaluator;
+
+internal class VallyUtilities
+{
+    public static async Task WritePromptsAsync(List<TestPrompt> prompts,
+        string outputFile,
+        string environment = "development",
+        bool force = false)
+    {
+        var serializer = new StaticSerializerBuilder(new VallyYamlStaticContext())
+            .EnsureRoundtrip()
+            .WithIndentedSequences()
+            .WithNamingConvention(HyphenatedNamingConvention.Instance)
+            .Build();
+
+        var stimuli = new List<Stimulus>();
+        for (int i = 0; i < prompts.Count; i++)
+        {
+            var p = prompts[i];
+            var graders = new List<StimulusGraderConfig>()
+            {
+                GetToolCallGrader(p.Namespace, p.Tool)
+            };
+            var stimulus = new Stimulus
+            {
+                Name = $"{p.Namespace} evaluation {i}",
+                Prompt = p.Prompt,
+                Environment = environment,
+                Graders = graders
+            };
+
+            stimuli.Add(stimulus);
+        }
+
+        var section = prompts[0].Section;
+        var evaluation = new Evaluation
+        {
+            Name = $"{section} evaluations",
+            Description = "Evaluation of prompts in the section " + section,
+            Stimuli = stimuli
+        };
+
+        var serialized = serializer.Serialize(evaluation);
+
+        if (File.Exists(outputFile))
+        {
+            if (!force)
+            {
+                throw new InvalidOperationException($"Output file {outputFile} already exists.");
+            } else
+            {
+                File.Delete(outputFile);
+            }
+        }
+
+        await File.WriteAllTextAsync(outputFile, serialized);
+    }
+
+    private static StimulusGraderConfig GetToolCallGrader(string toolName, string toolCommand)
+    {
+        var graderConfig = new GraderConfigEntry()
+        {
+            Required = [new GraderConfigEntryPair
+            {
+                Name = toolName,
+                Command = toolCommand
+            }]
+        };
+
+        return new StimulusGraderConfig
+        {
+            Type = "tool-calls",
+            Config = graderConfig
+        };
+    }
+}
