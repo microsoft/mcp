e7209ac5e8cc38c09498d164b1e7778a4906176f
Connie Yau <conniey@microsoft.com>
2026-07-22 11:31:07 -0700

    Add documentation to run config

--- eng/tools/VallyEvaluator/src/RunConfiguration.cs
+++ eng/tools/VallyEvaluator/src/RunConfiguration.cs
@@ -4,11 +4,17 @@ namespace VallyEvaluator;
 
 public class RunConfiguration
 {
+    /// <summary>
+    /// Tool namespaces to create evaluations for. Comma-separated list. For example: "storage,acr"
+    /// </summary>
     [ConfigurationKeyName("namespaces")]
     public string NamespacesValue { get; set; } = string.Empty;
 
     public List<string> Namespaces { get; set; } = new List<string>();
 
+    /// <summary>
+    /// Path to prompts file. If not set, will default to /servers/Azure.Mcp.Server/docs/e2eTestPrompts.md
+    /// </summary>
     [ConfigurationKeyName("promptFile")]
     public string PromptFilePath { get; set; } = string.Empty;
 
