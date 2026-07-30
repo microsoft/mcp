c039138196a284c5b5607839a6cc748b702f8136
Marcos Salamanca <masalama@microsoft.com>
2026-07-30 14:56:32 -0500

    update applens urls

--- tools/Azure.Mcp.Tools.AppLens/src/Services/AppLensService.cs
+++ tools/Azure.Mcp.Tools.AppLens/src/Services/AppLensService.cs
@@ -531,10 +531,10 @@ public class AppLensService(
     {
         return _tenantService.CloudConfiguration.CloudType switch
         {
-            AzureCloudConfiguration.AzureCloud.AzurePublicCloud => "https://diagnosticschat.azure.com/chatHub",
-            AzureCloudConfiguration.AzureCloud.AzureChinaCloud => "https://diagnosticschat.azure.cn/chatHub",
-            AzureCloudConfiguration.AzureCloud.AzureUSGovernmentCloud => "https://diagnosticschat.azure.us/chatHub",
-            _ => "https://diagnosticschat.azure.com/chatHub",
+            AzureCloudConfiguration.AzureCloud.AzurePublicCloud => "https://diagnosticschatnext.azure.com/chatHub",
+            AzureCloudConfiguration.AzureCloud.AzureChinaCloud => "https://diagnosticschatnext.azure.cn/chatHub",
+            AzureCloudConfiguration.AzureCloud.AzureUSGovernmentCloud => "https://diagnosticschatnext.azure.us/chatHub",
+            _ => "https://diagnosticschatnext.azure.com/chatHub",
         };
     }
 
